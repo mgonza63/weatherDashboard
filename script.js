243848951702cbe00a58cafbcde8f7728611cb55
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-01-29 16:16:17 -0600

    able call the UV index with ajax

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ $(document).ready(function() {
     
         // Here we are building the URL we need to query the database
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + APIKey;
+
+        
     
         // We then created an AJAX call
         $.ajax({
@@ -24,6 +26,28 @@ $(document).ready(function() {
           $("#temp").text("Temperature (F) " + response.main.temp);
           $("#humid").text("Humidity: " + response.main.humidity);
           $("#wind").text("Wind Speed: " + response.wind.speed);
+
+          var lon = response.coord.lon;
+          var lat = response.coord.lat;
+          var uvKey = "d68917803103c196cc3a57791401fdc9"
+
+          var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + uvKey + "&lat=" + lat + "&lon=" + lon;
+
+          $.ajax({
+            url: uvURL,
+            method: "GET"
+          }).then(function(data) {
+              console.log(data);
+              console.log(uvURL);
+              $("#uv").text("UV Index: " + data.value);
+        })
+
+
+
+          // $.get("https://api.openweathermap.org/data/2.5/uvi/forecast?appid=" + APIKey + "lat=" + lat + "&" + "lon=" + lon), function(data) {
+           //   $("#uv").text("UV Index: " + data.value); }
+
+        
     })
 
 
